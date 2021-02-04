b637aae2497b6087cb794c60aaa1f11fec9676c3
zkfy <lib4net@163.com>
2021-02-04 16:40:58 +0800

    测试新产品

--- otsserver/modules/const/sql/spp.product.go
+++ otsserver/modules/const/sql/spp.product.go
@@ -22,7 +22,7 @@ and t.brand_no = @brand_no
 and t.province_no = @province_no
 and t.city_no = @city_no
 and case when @can_split_order = 0 then t.face <= @face else t.face = @face end
-and t.cost_discount < @cost_discount
+and t.cost_discount <= @cost_discount
 and t.status = 0
 and s.status = 0
 order by t.cost_discount asc
