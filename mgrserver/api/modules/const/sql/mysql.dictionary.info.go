e6190b56b5b92e6a500f6202ccb5f9910ee74a1b
zkfy <lib4net@163.com>
2021-02-05 16:55:38 +0800

    更新逻辑

---
+++ mgrserver/api/modules/const/sql/mysql.dictionary.info.go
@@ -0,0 +1,73 @@
+package sql
+//InsertDictionaryInfo 添加字典配置
+const InsertDictionaryInfo = `
+insert into dds_dictionary_info
+(
+	
+	name,
+	value,
+	type,
+	status,
+	sort_no
+)
+values
+(
+	
+	@name,
+	@value,
+	@type,
+	if(isnull(@status)||@status='',0,@status),
+	if(isnull(@sort_no)||@sort_no='',0,@sort_no),
+)`
+
+//GetDictionaryInfoByID 查询单条数据字典配置
+const GetDictionaryInfoByID = `
+select
+	t.id,
+	t.name,
+	t.value,
+	t.type,
+	t.status,
+	t.sort_no
+from dds_dictionary_info t
+where
+	&id`
+
+//GetDictionaryInfoListCount 获取字典配置列表条数
+const GetDictionaryInfoListCount = `
+select count(1)
+from dds_dictionary_info t
+where
+	?t.name
+	&t.type
+	&t.status`
+
+//GetDictionaryInfoList 查询字典配置列表数据
+const GetDictionaryInfoList = `
+select
+	t.id,
+	t.name,
+	t.value,
+	t.type,
+	t.status,
+	t.sort_no 
+from dds_dictionary_info t
+where
+	?t.name
+	&t.type
+	&t.status
+order by t.id desc
+limit @ps offset @offset
+`
+//UpdateDictionaryInfoByID 更新字典配置
+const UpdateDictionaryInfoByID = `
+update dds_dictionary_info 
+set
+	name =	@name,
+	value =	@value,
+	type =	@type,
+	status =	if(isnull(@status)||@status='',0,@status),
+	sort_no =	if(isnull(@sort_no)||@sort_no='',0,@sort_no),
+where
+	&id`
+
