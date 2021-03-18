208b31d37b965a6b8f1fdede79b3beaa6bb283d9
zkfy <lib4net@163.com>
2021-03-18 20:44:38 +0800

    更新逻辑

--- flowserver/modules/biz/orders/replenish.sql.go
+++ flowserver/modules/biz/orders/replenish.sql.go
@@ -14,8 +14,7 @@ var reps = []string{
 	t.order_id,
 	t.order_status
 	from ots_trade_order t
-	where t.last_update_time > DATE_ADD(now(),INTERVAL -1 hour)
-	and  t.last_update_time < DATE_ADD(now(),INTERVAL -30 minute)
+	where t.last_update_time > DATE_ADD(now(),INTERVAL -1 minute)
 	and (t.order_status in(10,50,60) or ((t.total_face - t.bind_face)>0 and t.order_status = 20))
 	and t.batch_id=@batch_id
 	limit 20
